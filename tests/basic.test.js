0aef6d7a3ddbae347c4a4d228b7228b3526f715c
Md Hasibul Hasan Rahat <hasibrahat10@gmail.com>
2021-04-21 13:01:34 +0600

    TestCafe Environment  Set
    1. Environment set with dependency
    2. Add prettier
    3. Writing test script
    4. Execute the first test Cafe script
    5. Scripting in the package file with headless mode and different browser

---
+++ tests/basic.test.js
@@ -0,0 +1,15 @@
+import {Selector} from 'testcafe'
+
+//prettier-ignore
+fixture `Getting started with TestCafe`
+    .page`https://devexpress.github.io/testcafe/example/`
+
+
+test('My first testcafe test', async t=> {
+
+    await t.typeText('#developer-name','John')
+    await t.click('#submit-button')
+
+
+    await t.expect(Selector('#article-header').innerText).contains('John')
+})
