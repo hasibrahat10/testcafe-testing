e7e6633b3d259f3d930d3b0667da2c61682f717f
Md Hasibul Hasan Rahat <hasibrahat10@gmail.com>
2021-04-21 14:16:31 +0600

    Wait Hook Hooks
    1. Mobile emulation
    2. Hooks
    3. Test Speed
    4. Wait

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -3,11 +3,31 @@ import {Selector} from 'testcafe'
 //prettier-ignore
 fixture `Getting started with TestCafe`
     .page`https://devexpress.github.io/testcafe/example/`
+    .before(async t=>{
+        //Test setup
+        //await runDatabaseReset(), sendTestData()
+    })
 
+    .beforeEach(async t=>{
+        //Runs before each test
+
+        await t.setTestSpeed(1)
+    })
+
+    .after(async t=>{
+        //Cleaning test data 
+        // Logging and sending data to monitoring system
+    })
+
+    .afterEach(async t=>{
+        //Runs after each test
+    })
 
 test('My first testcafe test', async t=> {
 
+
     await t.typeText('#developer-name','John')
+    await t.wait(3000)
     await t.click('#submit-button')
 
 
